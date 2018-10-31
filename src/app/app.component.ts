3a694e5000ea08689e080d5ae2e4c2dffd6892d2
Fernando <ferry.fernando@gmail.com>
2018-10-31 15:45:52 +0700

    Finished implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsApiService } from './news-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'news-api';
+export class AppComponent implements OnInit {
+  mArticles: Array<any>;
+  mSources: Array<any>;
+
+  constructor(private newsapi: NewsApiService) {
+    console.log('app component constructor called');
+  }
+
+  ngOnInit() {
+    this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
+    this.newsapi.initSources().subscribe(data => this.mSources = data['sources']);
+  }
+
+  searchArticles(source) {
+    console.log('selected source is: ' + source);
+    this.newsapi.getArticleByID(source).subscribe(data => this.mArticles = data['articles']);
+  }
 }
