f9bc1fde8e99bad378b3aa8af0c10e47cf06af0a
Fernando <ferry.fernando@gmail.com>
2018-10-31 15:23:04 +0700

    News API implementation.

--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -7,5 +7,15 @@ import { HttpClient } from '@angular/common/http';
 export class NewsApiService {
   api_key = 'd27f55bb18f8475299781bf1df8cc23c';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  initSources() {
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
+  }
+  initArticles() {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
+  }
+  getArticleByID(source: String) {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  }
 }
